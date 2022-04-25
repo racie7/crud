c47eecc3117a7b8dcb8132e2e2cfca3394dd7adb
graciah <graciamuts@gmail.com>
2022-04-26 01:08:03 +0300

    add textfield place holders

--- student/forms.py
+++ student/forms.py
@@ -10,7 +10,7 @@ class StudentForm(ModelForm):
         fields = ('stud_name', 'stud_email', 'stud_contact', 'stud_course', 'stud_stud_id', 'image')
 
         widgets = {
-            'stud_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'stud_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Full Name'}),
             'stud_email': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
             'stud_contact': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone No.(In this format 254712313012)'}),
             'stud_course': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Course'}),
